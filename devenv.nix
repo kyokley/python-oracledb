d21a16686915035ddda6ca25bc6fb1a72ac8e5e2
Kevin Yokley <kyokley@mars>
2026-01-04 15:35:51 -0600

    Manage test db through devenv

--- devenv.nix
+++ devenv.nix
@@ -1,14 +1,33 @@
-{ pkgs, lib, config, inputs, ... }:
-
 {
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
+  env = {
+    GREET = "python-oracledb";
+    ORACLE_PASSWORD = "password";
+    ORACLE_LISTEN_ADDRESS = "127.0.0.1";
+    ORACLE_LISTEN_PORT = 1521;
+    ORACLE_CONTAINER_NAME = "oracledb";
+    ORACLE_APP_USER = "my_user";
+    ORACLE_APP_USER_PASSWORD = "password";
+    USE_HOST_NET = 1;
+  };
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [pkgs.docker];
 
   # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+  languages = {
+    python = {
+      enable = true;
+      version = "3.13";
+      uv.enable = true;
+    };
+  };
 
   # https://devenv.sh/processes/
   # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
@@ -17,14 +36,58 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
+  scripts = {
+    hello.exec = ''
+      echo $GREET
+    '';
+    up.exec = let
+      initScriptFile = builtins.toFile "01_create.sql" ''
+        ALTER SESSION SET current_schema = ${config.env.ORACLE_APP_USER};
+
+        CREATE TABLE student (
+            last_name       VARCHAR2(15) NOT NULL,
+            first_name      VARCHAR2(15) NOT NULL,
+            id              NUMBER(6) PRIMARY KEY
+        );
+        INSERT INTO student (last_name, first_name, id)
+        VALUES ('Doe', 'John', 1001);
+        SELECT * FROM student;
+      '';
+    in ''
+      set -x
+      : ''${USE_HOST_NET:=0}
+      if [ $USE_HOST_NET -eq 1 ]
+      then
+        DOCKER_NET_ARGS="--net=host"
+      else
+        DOCKER_NET_ARGS="-p ${config.env.ORACLE_LISTEN_ADDRESS}:${toString config.env.ORACLE_LISTEN_PORT}:1521"
+      fi
+
+      docker run \
+        --rm \
+        -d \
+        -t \
+        --name ${config.env.ORACLE_CONTAINER_NAME} \
+        -v ${toString initScriptFile}:/container-entrypoint-initdb.d/init.sql \
+        ''${DOCKER_NET_ARGS} \
+        -e ORACLE_PASSWORD="${config.env.ORACLE_PASSWORD}" \
+        -e APP_USER="${config.env.ORACLE_APP_USER}" \
+        -e APP_USER_PASSWORD="${config.env.ORACLE_APP_USER_PASSWORD}" \
+        gvenzl/oracle-free
+
+      logs
+    '';
+    logs.exec = ''
+      docker logs --follow ${config.env.ORACLE_CONTAINER_NAME}
+    '';
+    down.exec = ''
+      docker stop ${config.env.ORACLE_CONTAINER_NAME}
+    '';
+  };
 
   # https://devenv.sh/basics/
   enterShell = ''
     hello         # Run scripts directly
-    git --version # Use packages
   '';
 
   # https://devenv.sh/tasks/
@@ -36,7 +99,7 @@
   # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
+    echo "Done"
   '';
 
   # https://devenv.sh/git-hooks/
